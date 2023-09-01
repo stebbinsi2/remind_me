58ef0415191a2353c9fcc1ca2cf409f51d957892
stebbinsi2 <stebbinsi2@gmail.com>
2023-09-01 14:20:28 -0400

    removed alias

--- lib/remind_me/workers/reminder_email.ex
+++ lib/remind_me/workers/reminder_email.ex
@@ -1,6 +1,7 @@
 defmodule RemindMe.Workers.ReminderEmail do
   use Oban.Worker, queue: :default, max_attempts: 1
 
+
   @impl true
   def perform(_params) do
     RemindMe.EmailReminders.send_to_subscribers()
