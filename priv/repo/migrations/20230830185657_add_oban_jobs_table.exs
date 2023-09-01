0937adf15f433dbda36094312b32c2dccd815126
stebbinsi2 <stebbinsi2@gmail.com>
2023-09-01 13:52:52 -0400

    ObanJobsTable

--- priv/repo/migrations/20230830185657_add_oban_jobs_table.exs
+++ priv/repo/migrations/20230830185657_add_oban_jobs_table.exs
@@ -1,11 +1,11 @@
 defmodule RemindMe.Repo.Migrations.AddObanJobsTable do
   use Ecto.Migration
 
-    def up do
-      Oban.Migration.up(version: 11)
-    end
-    
-    def down do
-      Oban.Migration.down(version: 1)
+  def up do
+    Oban.Migration.up(version: 11)
+  end
+
+  def down do
+    Oban.Migration.down(version: 1)
   end
 end
