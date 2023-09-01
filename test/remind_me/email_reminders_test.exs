bd5fb31f1ef3c4a1eb0865e670a9f770c14fb6e5
stebbinsi2 <stebbinsi2@gmail.com>
2023-09-01 14:20:53 -0400

    Test suite for EmailReminders.build

--- test/remind_me/email_reminders_test.exs
+++ test/remind_me/email_reminders_test.exs
@@ -6,11 +6,58 @@ defmodule RemindMe.EmailRemindersTest do
   import RemindMe.AccountsFixtures
   import RemindMe.RemindersFixtures
 
-  test "send_to_subscribers" do
+  test "send_to_subscribers/0 _ one user _ one reminder" do
     user = user_fixture()
     reminder = reminder_fixture(user_id: user.id, subscribed: true, remind_date: Date.utc_today())
 
     EmailReminders.send_to_subscribers()
     assert_email_sent(EmailReminders.build(user.email, [reminder]))
+  end
+
+  test "send_to_subscribers/0 _ one user _ two reminders" do
+    user = user_fixture()
+    reminder1 = reminder_fixture(user_id: user.id, subscribed: true, remind_date: Date.utc_today())
+    reminder2 = reminder_fixture(user_id: user.id, subscribed: true, remind_date: Date.utc_today())
+
+    EmailReminders.send_to_subscribers()
+    assert_email_sent(EmailReminders.build(user.email, [reminder1, reminder2]))
+  end
+
+  test "send_to_subscribers/0 _ two users _ one reminder each" do
+    user1 = user_fixture()
+    user2 = user_fixture()
+    reminder1 = reminder_fixture(user_id: user1.id, subscribed: true, remind_date: Date.utc_today())
+    reminder2 = reminder_fixture(user_id: user2.id, subscribed: true, remind_date: Date.utc_today())
+
+    EmailReminders.send_to_subscribers()
+    assert_email_sent(EmailReminders.build(user1.email, [reminder1]))
+    assert_email_sent(EmailReminders.build(user2.email, [reminder2]))
+  end
+
+  test "send_to_subscribers/0 _ two users _ two reminders each" do
+    user1 = user_fixture()
+    user2 = user_fixture()
+    reminder1 = reminder_fixture(user_id: user1.id, subscribed: true, remind_date: Date.utc_today())
+    reminder2 = reminder_fixture(user_id: user1.id, subscribed: true, remind_date: Date.utc_today())
+    reminder3 = reminder_fixture(user_id: user2.id, subscribed: true, remind_date: Date.utc_today())
+    reminder4 = reminder_fixture(user_id: user2.id, subscribed: true, remind_date: Date.utc_today())
+
+    EmailReminders.send_to_subscribers()
+    assert_email_sent(EmailReminders.build(user1.email, [reminder1, reminder2]))
+    assert_email_sent(EmailReminders.build(user2.email, [reminder3, reminder4]))
+  end
+
+  test "send_to_subscribers/0 _ two users _ two reminders each _ one reminder not subscribed" do
+    user1 = user_fixture()
+    user2 = user_fixture()
+    reminder1 = reminder_fixture(user_id: user1.id, subscribed: true, remind_date: Date.utc_today())
+    reminder2 = reminder_fixture(user_id: user1.id, subscribed: true, remind_date: Date.utc_today())
+    reminder3 = reminder_fixture(user_id: user2.id, subscribed: false, remind_date: Date.utc_today())
+    reminder4 = reminder_fixture(user_id: user2.id, subscribed: true, remind_date: Date.utc_today())
+
+    EmailReminders.send_to_subscribers()
+    assert_email_sent(EmailReminders.build(user1.email, [reminder1, reminder2]))
+    assert_email_sent(EmailReminders.build(user2.email, [reminder4]))
+    assert_email_not_sent(EmailReminders.build(user2.email, [reminder3]))
   end
 end
